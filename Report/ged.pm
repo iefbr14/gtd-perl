b13b3afafd62ea0409a200793a8ea61c8c1786c8
Drew Sullivan <drew@ss.org>
2012-06-13 07:01:06 -0400

    Auto-commit silica.ss.org

---
+++ Report/ged.pm
@@ -0,0 +1,382 @@
+package Hier::Report::ged;
+
+use strict;
+use warnings;
+
+BEGIN {
+	use Exporter   ();
+	our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
+
+	# set the version for version checking
+	$VERSION     = 1.00;
+	@ISA         = qw(Exporter);
+	@EXPORT      = qw(&Report_ged);
+}
+
+use Hier::util;
+use Hier::Walk;
+use Hier::Resource;
+use Hier::Meta;
+use Hier::Filter;
+use Hier::Format;
+use Hier::Option;	# get_today
+
+my $ToOld;
+my $ToFuture;
+
+my $Someday = 0;
+
+sub Report_ged {	#-- generate taskjuggler file from gtd db
+	my($tid, $task, $cat, $ins, $due, $desc);
+
+	$ToOld = pdate(get_today(-7));	# don't care about done items > 2 week
+
+	if (scalar(@ARGV) && $ARGV[0] eq 'all') {
+		$Someday = 1;
+		meta_filter('+all', '^focus', 'none');
+		# 5 year plan everything plan
+		$ToFuture = pdate(get_today(5*365));	
+	} else {
+		meta_filter('+live', '^focus', 'none');
+		# don't care about start more > 3 months
+		$ToFuture = pdate(get_today(60));	
+	}
+	meta_argv(@ARGV);
+	my($planner) = new Hier::Walk;
+	$planner->set_depth('a');
+	$planner->filter();
+
+	ged_header();
+
+	bless $planner;
+	$planner->walk('m');
+	
+	ged_footer();
+}
+
+sub calc_est {
+	return 180;
+}
+
+sub ged_header {
+	my($date) = ged_date(get_today(0));
+	my $est = calc_est();
+	my $projection = pdate(get_today($est));	
+print <<"EOF";
+0 HEAD
+1   SOUR GTD
+2     VERS 0.0
+2     NAME GTD-perl GED Module
+1   DEST GEDCOM 5.5
+1   DATE $date
+1   CHAR UTF-8
+1   SUBM \@SUBM@
+1   COPR Copyright (c) $date
+1   GEDC
+2      VERS 5.5
+2      FORM LINEAGE-LINKED
+EOF
+
+}
+
+sub ged_footer {
+	print "0 TRLR\n";
+}
+
+sub header {
+	hier_detail(@_);
+}
+
+sub task_detail {
+	hier_detail(@_);
+}
+
+sub hier_detail {
+	my($planner, $ref) = @_;
+	my($sid, $name, $cnt, $desc, $type, $note);
+	my($per, $start, $end, $done, $due, $we);
+	my($who, $doit, $role, $depends);
+	my($birth);
+	my($tj_pri);
+
+	my($tid) = $ref->get_tid();
+
+	my($resource) = new Hier::Resource($ref);
+	
+	$name = $ref->get_task() || '';
+	$tj_pri  = task_priority($ref);
+	$desc = summary_line($ref->get_description(), '', 1);
+	$note = summary_line($ref->get_note(), '', 1);
+	$type = $ref->get_type() || '';
+	$per  = $ref->get_completed() ? 100 : 0;
+	$due  = pdate($ref->get_due());
+	$done = pdate($ref->get_completed());
+	$start = pdate($ref->get_tickledate());
+	$birth = pdate($ref->get_created());
+	$doit = pdate($ref->get_doit());
+	$depends = $ref->get_depends();
+
+	$role = $resource->resource($ref);
+
+	return if $type eq 'C'; # Checklists
+	return if $type eq 'L'; # Lists
+	return if $type eq 'T'; # Item
+
+	$who = 'drew';
+
+	my($effort) = $resource->effort($ref);
+
+	$due = '' if $due && $due lt '2010-';
+	$we    = $due || '';
+
+	my($fd) = $planner->{fd};
+
+	$name =~ s=/=.=g;
+#	print {$fd} qq(task $type\_$tid "$name" \{\n);
+
+#	if ($type eq 'o') {
+#		print {$fd} qq(   start \${now}\n);
+#		print {$fd} qq(   allocate $role\n);
+#	} elsif ($role && parent_role($ref) ne $role) {
+#		print {$fd} qq(   allocate $role { mandatory }\n);
+#	}
+
+	foreach my $depend (split(/[ ,]/, $depends)) {
+		my($dep_path) = dep_path($depend);
+
+		unless ($dep_path) {
+			warn "depend $tid: needs $depend failed to produce path!";
+			next;
+		}
+		if ($dep_path =~ /^\s*#/) {
+			warn "depend $tid: no-longer depends: $depend $dep_path\n";
+			next;
+		}
+
+		warn "depend $tid: $depend dep_path $dep_path\n";
+#		print {$fd} qq(   depends $dep_path\n);
+	}
+
+
+	print {$fd} "0 ".id('I',$ref)." INDI\n";
+
+	$role = ucfirst($role);
+
+	print {$fd} "  1 NAME $name /$role/\n";
+
+   if ($desc) {
+	print {$fd} "  1 DESC $desc\n";
+   }
+   if ($note) {
+	print {$fd} "  1 CAST $note\n";
+   }
+
+	print {$fd} "  1 BIRT\n";
+	print {$fd} "    2 DATE ", ged_date($birth),"\n";
+
+   if ($start) {
+	print {$fd} "  1 BAPM\n";
+	print {$fd} "    2 DATE ", ged_date($start),"\n";
+   }
+   if ($done) {
+	print {$fd} "  1 CONF\n";
+	print {$fd} "    2 DATE ", ged_date($doit),"\n";	
+   }
+   if ($due) {
+	print {$fd} "  1 GRAD\n";
+	print {$fd} "    2 DATE ", ged_date($due),"\n";
+   }
+   if ($done) {
+	print {$fd} "  1 DEAT\n";
+	print {$fd} "    2 DATE ", ged_date($done),"\n";	
+   }
+
+	my($boost) = $ref->is_nextaction() ? 'M' : 'F';
+	print {$fd} "  1 SEX $boost\n";
+
+#	print {$fd} qq(   effort $effort\n) if $effort;
+#	print {$fd} qq(   priority $tj_pri\n) if $tj_pri;
+#	
+#	print {$fd} qq(   start $start\n) if $start && $we eq '';
+#	print {$fd} qq(   maxend  $we\n)   if $we;
+#	print {$fd} qq(   complete  100\n)   if $done;
+
+
+   if ($done) {
+	print {$fd} "  1 BIRT\n";
+	print {$fd} "    1 DATE", ged_date($done),"\n";	
+  }
+
+	my(@children) = $ref->get_children();
+	return unless @children;
+
+	print {$fd} "   1 FAMC ".id('F',$ref)."\n";	
+
+	print {$fd} "0 ".id('F',$ref)." FAM\n";	
+	my $n = 1;
+	for my $child (@children) {
+		print {$fd} "  $n CHIL ", id('I',$child), "\n";
+		++$n;
+	}
+}
+
+sub end_detail {
+}
+
+sub id {
+	my($t, $ref) = @_;
+
+	my($tid) = $ref->get_tid();
+
+	return sprintf "\@%s%04d\@", uc($t), $tid;
+}
+
+sub pdate {
+	my($date) = @_;
+
+	return '' if $date eq '';
+	return '' if $date =~ /^0000/;
+
+	$date =~ s/ .*$//;
+	return ged_date($date);
+}
+
+sub parent_role {
+	my($ref) = @_;
+
+	my($pref) = $ref->get_parent();
+	return '' unless $pref;
+
+	my($resource) = new Hier::Resource($pref);
+	return $resource->resource($pref);
+}
+
+sub dep_path {
+	my($tid) = @_;
+
+	my($ref) = meta_find($tid);
+	return unless $ref;
+
+	my($task) = $ref->get_task($ref);
+	my($path) = $ref->get_type() . '_' . $tid;
+	my($pref);
+
+	if ($ref->get_completed()) {
+		return "# depends on $tid ($task) is done";
+	}
+
+	for (;;) {
+		$ref = $ref->get_parent();
+		last unless $ref;
+
+		$path = $ref->get_type() . '_' . $ref->get_tid() . '.' . $path;
+		last if $ref->get_type() eq 'o';
+	}
+	return $path . " # $task";
+}
+
+
+sub supress {
+	my($planner, $ref) = @_;
+
+	my($tid) = $ref->get_tid();
+	$planner->{want}{$tid} = 0;
+
+	foreach my $child ($ref->get_children()) {
+		supress($planner, $child);
+	}
+}
+
+sub old_task_priority {
+	my($ref) = @_;
+
+	my($pri) = $ref->get_priority();
+
+	return '' unless $pri;
+
+	my($type) = $ref->get_type();
+	return '' if $type eq 'o';
+	return '' if $type eq 'g';
+	return '' if $type eq 'p';
+
+	my($boost) = $ref->is_nextaction();
+
+	my($prival) = '';
+
+	for (;;) {
+		$pri = $ref->get_priority();
+		$pri += 4 if $ref->is_someday();
+		$prival = $pri . $prival;
+
+		last if $ref->get_type() eq 'g';
+
+		$ref = $ref->get_parent();
+		last unless $ref;
+		last if $ref->get_type() eq 'g';
+	}
+
+	return '' if $prival =~ /^4+$/;	 # all defaults
+
+	my($tj_pri) = 1100 - int(('.' . $prival) * 1000);
+
+	$tj_pri = 1 if $tj_pri <= 0;
+
+	if ($type eq 'a' && $boost) {
+		$tj_pri += 100;
+		$tj_pri = 999 if $tj_pri >= 1000;
+	}
+	return $tj_pri . " # $prival.$boost";
+}
+sub task_priority {
+	my($ref) = @_;
+
+	my($pri) = $ref->get_priority();
+	$pri += 4 if $ref->is_someday();
+
+	return '' unless $pri;
+	return '' if $pri == 4;
+
+	my($type) = $ref->get_type();
+#	return '' if $type eq 'o';
+#	return '' if $type eq 'g';
+#	return '' if $type eq 'p';
+
+	my($boost) = $ref->is_nextaction();
+
+	my($tj_pri) = (1000 - ($pri*100)) + $boost*50;
+
+	$tj_pri = 1000 if $tj_pri >= 1000;
+	$tj_pri = 1 if $tj_pri <= 0;
+
+	return $tj_pri . " # $pri.$boost";
+}
+
+sub ged_date {
+	my($date) = @_;
+
+	return '' unless $date;
+
+	$date =~ tr=/=-=;
+	my($y, $m, $d) = split('-', $date);
+
+	return $date unless $d;
+
+	$m = qw(JAN FEB MAR APR MAY JUN JUL AUG SEP OCT NOV DEC)[$m-1];
+
+	return "$d $m $y";
+}
+
+sub note {
+	my($note) = @_;
+
+	my $res = '1 NOTE ';
+	while (length($note) > 72) {
+		$res .= substr($note, 0, 72) . "\n2 CONT ";
+		$note = substr($note, 72);
+	}
+	$res .= $note . "\n";;
+	return $res;
+
+}
+
+1;  # don't forget to return a true value from the file
