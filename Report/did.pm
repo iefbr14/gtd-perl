8287e45e6f5f1daf6c795c91a77d3bfaacb75a49
Drew Sullivan <drew@ss.org>
2012-12-08 07:01:50 -0500

    Auto-commit silica.ss.org

---
+++ Report/did.pm
@@ -0,0 +1,32 @@
+package Hier::Report::did;
+
+use strict;
+use warnings;
+
+BEGIN {
+	use Exporter   ();
+	our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
+
+	# set the version for version checking
+	$VERSION     = 1.00;
+	@ISA         = qw(Exporter);
+	@EXPORT      = qw(&Report_did);
+}
+
+use Hier::Option;
+use Hier::Meta;
+
+sub Report_did {	#-- update listed projects/actions doit date to today
+	for my $tid (@_) {
+		my $ref = meta_find($tid);
+
+		unless (defined $ref) {
+			print "Task $tid not found to tag done\n";
+			next;
+		}
+		$ref->set_doit(get_today());
+		$ref->update();
+	}
+}
+
+1;  # don't forget to return a true value from the file
