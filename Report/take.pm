75fb482714100bcd7a18086078d68bf0d3dcc456
Drew Sullivan <drew@coral.ss.org>
2015-11-14 08:59:35 -0500

    Started coding take for taking ownership of a task

---
+++ Report/take.pm
@@ -0,0 +1,121 @@
+package Hier::Report::take;
+
+=head1 NAME
+
+=head1 USAGE
+
+=head1 REQUIRED ARGUMENTS
+
+=head1 OPTION
+
+=head1 DESCRIPTION
+
+=head1 DIAGNOSTICS
+
+=head1 EXIT STATUS
+
+=head1 CONFIGURATION
+
+=head1 DEPENDENCIES
+
+=head1 INCOMPATIBILITIES
+
+=head1 BUGS AND LIMITATIONS
+
+=head1 AUTHOR
+
+=head1 LICENSE and COPYRIGHT
+
+(C) Drew Sullivan 2015 -- LGPL 3.0 or latter
+
+=head1 HISTORY
+
+=cut
+
+use strict;
+use warnings;
+
+BEGIN {
+	use Exporter   ();
+	our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
+
+	# set the version for version checking
+	$VERSION     = 1.00;
+	@ISA         = qw(Exporter);
+	@EXPORT      = qw(&Report_take);
+}
+
+use Hier::Util;
+use Hier::Meta;
+
+use Hier::Option;
+use Hier::Format;
+
+my %Ancestors;
+
+sub Report_take {	#-- take listed actions/projects
+	my($key, $val, $changed);
+
+	meta_filter('+all', '^tid', 'none');
+
+	my $parent = option('Current');
+	unless ($parent) {
+		die "No parent for take\n";
+	}
+	my $p_ref = find($parent);
+	unless ($p_ref) {
+		die "Parent $parent doesn't exists\n";
+	}
+
+	my(@list) = meta_pick(@_);
+	if (@list == 0) {
+		die "No items to take\n";
+	}
+
+	get_ancestors($p_ref);
+	for my $child (@list) {
+		my($c_ref) = find($child);
+		unless ($c_ref) {
+			die "No such child $child\n";
+		}
+
+		my($tid) = is_ancestor($c_ref);
+		if ($tid) {
+			die "Child $tid shares ancestor for $parent\n";
+		}
+		print "Take $parent <= $child\n";
+	}
+}   
+
+sub get_ancestors {
+	my($ref) = @_;
+
+	my($tid) = $ref->get_tid();
+
+	$Ancestors{$tid} = $ref;
+
+	for my $parent ($ref->get_parents()) {
+		get_ancestors($parent);
+	}
+}
+
+sub is_ancestors {
+	my($ref) = @_;
+
+	for my $child ($ref->get_children()) {
+		my($tid) = $child->get_tid();
+
+		return $tid if defined $Ancestors{$tid};
+
+		# check my children recursivly as well
+		$tid = is_ancestor($child);
+		return $tid if $tid;	# yup.
+
+		# not this one, continue looking
+	}
+
+	# nope no child is an ancestor
+	return 0;
+}
+
+1;  # don't forget to return a true value from the file
