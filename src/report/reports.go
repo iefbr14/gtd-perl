ecc14de3e430365212c6a326eb2cb49a25f31d5c
Drew Sullivan <drew@coral.ss.org>
2016-01-06 12:17:12 -0500

    Completed conversion of reports.go

--- src/report/reports.go
+++ src/report/reports.go
@@ -33,321 +33,277 @@ NAME:
 
 */
 
-use Hier::Util;
-use Hier::Meta;
-use Hier::Format;
+import "gtd"
 
-our $OurPath;
-
-my($Fname) = 0;
-
-func Report_reports(args ...string) {	//-- List Reports
-	if (@_ && $_[0] =~ /^f/) {
-		$Fname = 1;
-	}
-	report_header('Reports');
-
-	my @files  = get_reports();
-
-	print join("\n", @files), "\n\n";
-}
-
-sub get_reports {
-	my(@list, $name);
-	
-	my($f, $path);
-	my($dir) = $OurPath;
-
-	$dir =~ s=/reports.pm==;
-	opendir(DIR, $dir) or panic("Can't open $dir ($!)\n");
-	while ($f = readdir(DIR)) {
-		next unless $f =~ /\.pm$/;
-
-		$path = "$dir/$f";
-
-		open(my $fd, "< $path") or die "Can't open $path ($!)\n";
-		while (<$fd>) {
-			next unless /^sub Report_(\w+)/;
-			$name = $1;
-			if ($Fname) {
-				if ($f =~ /^(.*)\.pm$/) {
-					$name = "$1\t$name";
-				} else {
-					$name = "$f\t$name";
-				}
-			}
-
-			if (m/#--\s*(.*)/) {
-				push(@list, sprintf("%-12s -- %s", $name, $1));
-			} else {
-				push(@list, $name);
-			}
-		}
-		close($fd);
-	}
-	return sort @list;
-}
-
-type reports struct {
-	name string,
+type Reports struct {
+	name string
 	f    func(...string) int
-	desc string,
+	desc string
 }
 
-var report_list []reports = {
-    {
-	"actions",
-	Report_actions,
-	"Detailed list of projects with (next) actions",
-    },
-    {
-	"addplans",
-	Report_addplans,
-	"add plan action items to unplaned projects",
-    },
-    {
-	"board",
-	Report_board,
-	"report board of projects/actions",
-    },
-    {
-	"bulk",
-	Report_bulk,
-	"Create Bulk create Projects/Actions items from a file",
-    },
-    {
-	"bulkload",
-	Report_bulkload,
-	"Create Projects/Actions items from a file",
-    },
-    {
-	"cct",
-	Report_cct,
-	"List Categories/Contexts/Time Frames",
-    },
-    {
-	"checklist",
-	Report_checklist,
-	"display a check list",
-    },
-    {
-	"clean",
-	Report_clean,
-	"clean unused categories",
-    },
-    {
-	"color",
-	Report_color,
-	"Detailed list of projects with (next) actions",
-    },
-    {
-	"delete",
-	Report_delete,
-	"Delete listed actions/projects (will orphine items)",
-    },
-    {
-	"did",
-	Report_did,
-	"update listed projects/actions doit date to today",
-    },
-    {
-	"doit",
-	Report_doit,
-	"doit tracks which projects/actions have had movement",
-    },
-    {
-	"done",
-	Report_done,
-	"Tag listed projects/actions as done",
-    },
-    {
-	"dump",
-	Report_dump,
-	"dump records in edit format",
-    },
-    {
-	"edit",
-	Report_edit,
-	"Edit listed actions/projects",
-    },
-    {
-	"fixcct",
-	Report_fixcct,
-	"Fix Categories/Contexts/Time Frames",
-    },
-    {
-	"focus",
-	Report_focus,
-	"List focus -- live, plan or someday",
-    },
-    {
-	"ged",
-	Report_ged,
-	"generate a gedcom file from gtd db",
-    },
-    {
-	"gui",
-	Report_gui,
-	"Tk gui front end",
-    },
-    {
-	"help",
-	Report_help,
-	"Help on commands",
-    },
-    {
-	"hier",
-	Report_hier,
-	"Hiericial List of Values/Visions/Roles...",
-    },
-    {
-	"hierlist",
-	Report_hierlist,
-	"List all top level item (Project and above)",
-    },
-    {
-	"init",
-	Report_init,
-	"Init ~/.todo structure",
-    },
-    {
-	"items",
-	Report_items,
-	"list titles for any filtered class (actions/projects etc)",
-    },
-    {
-	"kanban",
-	Report_kanban,
-	"report kanban of projects/actions",
-    },
-    {
-	"list",
-	Report_list,
-	"list titles for any filtered class (actions/projects etc)",
-    },
-    {
-	"merge",
-	Report_merge,
-	"Merge Projects (first list is receiver)",
-    },
-    {
-	"new",
-	Report_new,
-	"create a new action or project",
-    },
-    {
-	"nextactions",
-	Report_nextactions,
-	"List next actions",
-    },
-    {
-	"oocalc",
-	Report_oocalc,
-	"Project Summary for a role",
-    },
-    {
-	"orphans",
-	Report_orphans,
-	"list all items without a parent ",
-    },
-    {
-	"planner",
-	Report_planner,
-	"Create a planner file from gtd db",
-    },
-    {
-	"print",
-	Report_print,
-	"dump records in edit format",
-    },
-    {
-	"projects",
-	Report_projects,
-	"List projects -- live, plan or someday",
-    },
-    {
-	"purge",
-	Report_purge,
-	"interactive purge completed work",
-    },
-    {
-	"rc",
-	Report_rc,
-	"rc - Run Commands",
-    },
-    {
-	"records",
-	Report_records,
-	"detailed list all records for a type",
-    },
-    {
-	"renumber",
-	Report_renumber,
-	"Renumber task Ids ",
-    },
-    {
-	"review",
-	Report_review,
-	"Review all projects with actions",
-    },
-    {
-	"search",
-	Report_search,
-	"Search for items",
-    },
-    {
-	"spreadsheet",
-	Report_spreadsheet,
-	"Project Summary for a role",
-    },
-    {
-	"status",
-	Report_status,
-	"report status of projects/actions",
-    },
-    {
-	"take",
-	Report_take,
-	"take listed actions/projects",
-    },
-    {
-	"task",
-	Report_task,
-	"quick List by various methods",
-    },
-    {
-	"taskjuggler",
-	Report_taskjuggler,
-	"generate taskjuggler file from gtd db",
-    },
-    {
-	"todo",
-	Report_todo,
-	"List high priority next actions",
-    },
-    {
-	"toplevel",
-	Report_toplevel,
-	"List Values/Visions/Roles",
-    },
-    {
-	"tsort",
-	Report_tsort,
-	"write out hier as as set of nodes",
-    },
-    {
-	"update",
-	Report_update,
-	"Command line update of an action/project",
-    },
-    {
-	"url",
-	Report_url,
-	"open browser window for wiki and gtd",
-    },
-    {
-	"walk",
-	Report_walk,
-	"Command line walk of a hier",
-    },
+var report_list = []Reports{
+	{
+		"actions",
+		Report_actions,
+		"Detailed list of projects with (next) actions",
+	},
+	{
+		"addplans",
+		Report_addplans,
+		"add plan action items to unplaned projects",
+	},
+	{
+		"board",
+		Report_board,
+		"report board of projects/actions",
+	},
+	{
+		"bulk",
+		Report_bulk,
+		"Create Bulk create Projects/Actions items from a file",
+	},
+	{
+		"bulkload",
+		Report_bulkload,
+		"Create Projects/Actions items from a file",
+	},
+	{
+		"cct",
+		Report_cct,
+		"List Categories/Contexts/Time Frames",
+	},
+	{
+		"checklist",
+		Report_checklist,
+		"display a check list",
+	},
+	{
+		"clean",
+		Report_clean,
+		"clean unused categories",
+	},
+	{
+		"color",
+		Report_color,
+		"Detailed list of projects with (next) actions",
+	},
+	{
+		"delete",
+		Report_delete,
+		"Delete listed actions/projects (will orphine items)",
+	},
+	{
+		"did",
+		Report_did,
+		"update listed projects/actions doit date to today",
+	},
+	{
+		"doit",
+		Report_doit,
+		"doit tracks which projects/actions have had movement",
+	},
+	{
+		"done",
+		Report_done,
+		"Tag listed projects/actions as done",
+	},
+	{
+		"dump",
+		Report_dump,
+		"dump records in edit format",
+	},
+	{
+		"edit",
+		Report_edit,
+		"Edit listed actions/projects",
+	},
+	{
+		"fixcct",
+		Report_fixcct,
+		"Fix Categories/Contexts/Time Frames",
+	},
+	{
+		"focus",
+		Report_focus,
+		"List focus -- live, plan or someday",
+	},
+	{
+		"ged",
+		Report_ged,
+		"generate a gedcom file from gtd db",
+	},
+	{
+		"gui",
+		Report_gui,
+		"Tk gui front end",
+	},
+	{
+		"help",
+		Report_help,
+		"Help on commands",
+	},
+	{
+		"hier",
+		Report_hier,
+		"Hiericial List of Values/Visions/Roles...",
+	},
+	{
+		"hierlist",
+		Report_hierlist,
+		"List all top level item (Project and above)",
+	},
+	{
+		"init",
+		Report_init,
+		"Init ~/.todo structure",
+	},
+	{
+		"items",
+		Report_items,
+		"list titles for any filtered class (actions/projects etc)",
+	},
+	{
+		"kanban",
+		Report_kanban,
+		"report kanban of projects/actions",
+	},
+	{
+		"list",
+		Report_list,
+		"list titles for any filtered class (actions/projects etc)",
+	},
+	{
+		"merge",
+		Report_merge,
+		"Merge Projects (first list is receiver)",
+	},
+	{
+		"new",
+		Report_new,
+		"create a new action or project",
+	},
+	{
+		"nextactions",
+		Report_nextactions,
+		"List next actions",
+	},
+	{
+		"oocalc",
+		Report_oocalc,
+		"Project Summary for a role",
+	},
+	{
+		"orphans",
+		Report_orphans,
+		"list all items without a parent ",
+	},
+	{
+		"planner",
+		Report_planner,
+		"Create a planner file from gtd db",
+	},
+	{
+		"print",
+		Report_print,
+		"dump records in edit format",
+	},
+	{
+		"projects",
+		Report_projects,
+		"List projects -- live, plan or someday",
+	},
+	{
+		"purge",
+		Report_purge,
+		"interactive purge completed work",
+	},
+	{
+		"rc",
+		Report_rc,
+		"rc - Run Commands",
+	},
+	{
+		"records",
+		Report_records,
+		"detailed list all records for a type",
+	},
+	{
+		"renumber",
+		Report_renumber,
+		"Renumber task Ids ",
+	},
+	{
+		"review",
+		Report_review,
+		"Review all projects with actions",
+	},
+	{
+		"search",
+		Report_search,
+		"Search for items",
+	},
+	{
+		"spreadsheet",
+		Report_spreadsheet,
+		"Project Summary for a role",
+	},
+	{
+		"status",
+		Report_status,
+		"report status of projects/actions",
+	},
+	{
+		"take",
+		Report_take,
+		"take listed actions/projects",
+	},
+	{
+		"task",
+		Report_task,
+		"quick List by various methods",
+	},
+	{
+		"taskjuggler",
+		Report_taskjuggler,
+		"generate taskjuggler file from gtd db",
+	},
+	{
+		"todo",
+		Report_todo,
+		"List high priority next actions",
+	},
+	{
+		"toplevel",
+		Report_toplevel,
+		"List Values/Visions/Roles",
+	},
+	{
+		"tsort",
+		Report_tsort,
+		"write out hier as as set of nodes",
+	},
+	{
+		"update",
+		Report_update,
+		"Command line update of an action/project",
+	},
+	{
+		"url",
+		Report_url,
+		"open browser window for wiki and gtd",
+	},
+	{
+		"walk",
+		Report_walk,
+		"Command line walk of a hier",
+	},
+}
+
+//-- List Reports
+func Report_reports(args ...string) { 
+	report_header("Reports")
+
+	for v := range report_list {
+		display(fmt.Sprintf("%-12s -- %s", v.name, v.desc))
+	}
 }
